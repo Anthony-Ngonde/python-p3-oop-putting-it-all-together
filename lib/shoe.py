868200abea4790b16eff0a4a77232dbeb61a1c48
Anthony Ngonde <anto.ngonde@gmail.com>
2024-05-26 10:50:30 +0300

    Lab completed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        if isinstance(size, int):
+            self.size = size
+        else:
+            print("size must be an integer")
+        self.condition = 'New'
+
+    def repair(self):
+        print("The shoe has been repaired.")
+        self.condition = 'New'
